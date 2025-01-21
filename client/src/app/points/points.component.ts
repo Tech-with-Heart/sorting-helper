0bec2dfe7ecc656ce759877c48fad43a47ecdf10
Weronika Jankowska <>
2025-01-21 11:51:55 +0100

    init leaflet

--- client/src/app/points/points.component.ts
+++ client/src/app/points/points.component.ts
@@ -1,25 +1,62 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { PointMapper } from './mappers/point.mapper';
+import { Component, OnInit } from '@angular/core';
+import { LeafletModule } from '@bluehalo/ngx-leaflet';
+import {
+  Icon,
+  icon,
+  latLng,
+  Marker,
+  marker,
+  polygon,
+  tileLayer,
+  
+} from 'leaflet';
 
 @Component({
   selector: 'app-points',
-  imports: [],
+  imports: [LeafletModule],
   templateUrl: './points.component.html',
   styleUrl: './points.component.scss',
 })
 export class PointsComponent implements OnInit {
-  selectedPoint = signal(
-    PointMapper.fromDTO({
-      _id: '',
-      address: 'SKLEP ŻABKA - UMIŃSKIEGO 12',
-      description: 'Punkt, do którego można oddawać zużyte baterie i akumulatory.',
-      latitude: 52.394319,
-      longitude: 16.909855,
-      pointType: 'batteries',
-    })
-  );
+  // selectedPoint = signal(
+  //   PointMapper.fromDTO({
+  //     _id: '',
+  //     address: 'SKLEP ŻABKA - UMIŃSKIEGO 12',
+  //     description: 'Punkt, do którego można oddawać zużyte baterie i akumulatory.',
+  //     latitude: 52.394319,
+  //     longitude: 16.909855,
+  //     pointType: 'batteries',
+  //   })
+  // );
+
+  private defaultIcon = icon({
+    ...Icon.Default.prototype.options,
+    iconUrl: 'assets/marker-icon.png',
+    iconRetinaUrl: 'assets/marker-icon-2x.png',
+    shadowUrl: 'assets/marker-shadow.png',
+  });
+
+  private initLatLng = { lat: 52.394319, lng: 16.909855 }
+
+  mapInitOptions = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }),
+    ],
+    zoom: 10,
+    center: latLng(this.initLatLng),
+  };
+
+  mapLayers: Marker[] = [];
 
   ngOnInit() {
-    console.log('test', this.selectedPoint());
+    this.mapLayers = [
+      marker(this.initLatLng, {
+        icon: this.defaultIcon,
+      }).bindPopup('halko'),
+    ];
   }
 }
